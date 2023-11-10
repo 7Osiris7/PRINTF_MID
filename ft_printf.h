9adbe6b34a7b90e68cd1d9d9bd292a8066d08707
Jean Ruhlmann <jruhlman@k1r2p4.42mulhouse.fr>
2023-11-10 18:24:12 +0100

    Printf FT

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jruhlman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 17:10:17 by jruhlman          #+#    #+#             */
-/*   Updated: 2023/11/10 17:20:04 by jruhlman         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:22:04 by jruhlman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,11 @@ typedef struct	s_t
 }	t_t;
 
 int	ft_printf(const char *format, ...);
+int	ft_printchar(char c, t_t tt);
+int	ft_printstr(char *str, t_t tt);
+int	ft_printptr(void *ptr, t_t tt);
+int	ft_printnum(long num, t_t tt);
+int	ft_printunum(unsigned long num, t_t tt);
+int	ft_printbnum(int num, int is_upper, t_t tt);
 
 #endif
